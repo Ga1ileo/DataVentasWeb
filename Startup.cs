658de6b6fd1c472747f0743a1b01dfb7144e8c26
Ga1ileo <59858318+Ga1ileo@users.noreply.github.com>
2020-11-18 13:45:06 -0400

    Microsoft Login Comentado
    Microsoft Login Comentado

--- Startup.cs
+++ Startup.cs
@@ -68,11 +68,11 @@ namespace DataVentasWeb
                 options.ClientSecret = googleAuthNSection["ClientSecret"];
             });
 
-            services.AddAuthentication().AddMicrosoftAccount(microsoftOptions =>
-            {
-                microsoftOptions.ClientId = Configuration["Authentication:Microsoft:ClientId"];
-                microsoftOptions.ClientSecret = Configuration["Authentication:Microsoft:ClientSecret"];
-            });
+            //services.AddAuthentication().AddMicrosoftAccount(microsoftOptions =>
+            //{
+            //    microsoftOptions.ClientId = Configuration["Authentication:Microsoft:ClientId"];
+            //    microsoftOptions.ClientSecret = Configuration["Authentication:Microsoft:ClientSecret"];
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
