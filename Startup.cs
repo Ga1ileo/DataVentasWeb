2e1f530b66c857ab4f7c972a4b55c68356d7e45d
Ga1ileo <59858318+Ga1ileo@users.noreply.github.com>
2020-11-18 08:52:19 -0400

    Google Login

--- Startup.cs
+++ Startup.cs
@@ -58,6 +58,15 @@ namespace DataVentasWeb
                     }
                 };
             });
+
+            services.AddAuthentication().AddGoogle(options =>
+            {
+                IConfigurationSection googleAuthNSection =
+                Configuration.GetSection("Authentication:Google");
+
+                options.ClientId = googleAuthNSection["ClientId"];
+                options.ClientSecret = googleAuthNSection["ClientSecret"];
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
